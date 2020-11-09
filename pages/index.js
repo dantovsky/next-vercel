47cf4dff52ce78b8dc70492ca9af9cb9b39a2111
Dante Marinho <dantecnia@hotmail.com>
2020-11-09 20:05:56 +0000

    Index page.

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+
+function Home() {
+    return (
+        <div>
+            <h1>My Title</h1>
+            <Contador />
+        </div>
+    )
+}
+
+function Contador() {
+    const [contador, setContador] = useState(1)
+
+    function adicionarContador() {
+        setContador(contador + 1)
+    }
+
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+            <h2>SPA Frameworks</h2>
+            <ul>
+                <li>React</li>
+                <li>Vue</li>
+                <li>Angular</li>
+            </ul>
+        </div>
+    )
+}
+export default Home
