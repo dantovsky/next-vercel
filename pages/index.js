2dafe097aa2a70d4ce726560d41034e965fd3d0c
Dante Marinho <dantecnia@hotmail.com>
2020-11-09 20:46:58 +0000

    New commit

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { useState } from 'react'
 function Home() {
     return (
         <div>
-            <h1>My Title</h1>
+            <h1>My Title | Modifications</h1>
             <Contador />
         </div>
     )
